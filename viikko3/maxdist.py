1ec73ef2a9856d802c9b4ad2d8e9a72e565fc791
Henri Nikku <henrinikku1@gmail.com>
2021-09-19 22:03:43 +0300

    formatting

--- viikko3/maxdist.py
+++ viikko3/maxdist.py
@@ -4,8 +4,9 @@ from typing import List
 def find(t: List[int]):
     return max(t) - min(t)
 
+
 if __name__ == "__main__":
-    print(find([4,5,2,1,2])) # 4
-    print(find([1,2,3,4])) # 3
-    print(find([1])) # 0
-    print(find([1,2,100,5])) # 99
+    print(find([4, 5, 2, 1, 2]))  # 4
+    print(find([1, 2, 3, 4]))  # 3
+    print(find([1]))  # 0
+    print(find([1, 2, 100, 5]))  # 99
