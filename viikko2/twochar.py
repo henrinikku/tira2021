fb75d4d315af8159ebde4ee060f02092332eb35d
Henri Nikku <henrinikku1@gmail.com>
2021-09-19 21:52:24 +0300

    Add twochars.py

--- viikko2/twochar.py
+++ viikko2/twochar.py
@@ -1,5 +1,29 @@
+def _count(s: str, n: int):
+    char_count = {}
+    result = 0
+    subs_i = 0
+    subs_l = n
+    for i, c in enumerate(s):
+        if char_count.get(c, 0) == 0:
+            subs_l -= 1
+
+        char_count[c] = char_count.get(c, 0) + 1
+
+        while subs_l < 0:
+            subs_c = s[subs_i]
+            char_count[subs_c] = char_count.get(subs_c, 0) - 1
+            if char_count[subs_c] == 0:
+                subs_l += 1
+
+            subs_i += 1
+
+        result += i - subs_i + 1
+
+    return result
+
+
 def count(s: str):
-    pass
+    return _count(s, 2) - _count(s, 1)
 
 
 if __name__ == "__main__":
