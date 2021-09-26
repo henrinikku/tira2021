78fc4249e9b0279fb619a2e93990ef7bcf9a7f3d
Henri Nikku <henrinikku1@gmail.com>
2021-09-26 21:27:17 +0300

    Improve movesort based on model solution

--- viikko3/movesort.py
+++ viikko3/movesort.py
@@ -2,28 +2,10 @@ from typing import List
 
 
 def check(t: List[int]):
-    left_cursor = 0
-    right_cursor = len(t) - 1
-    while left_cursor < right_cursor:
-        if not (
-            (t[left_cursor] < t[left_cursor + 1])
-            == (t[right_cursor - 1] < t[right_cursor])
-        ):
-            return False
-
-        if not (
-            (t[left_cursor] == left_cursor + 1) == (t[right_cursor] == right_cursor + 1)
-        ):
-            return False
-
-        left_cursor += 1
-        right_cursor -= 1
-
     half = len(t) // 2
-    if t[half - 1] + 1 == t[half] and t[half - 1] != half:
-        return False
-
-    return True
+    left = t[:half]
+    right = reversed(t[half:])
+    return all(i + next(right) == len(t) + 1 for i in left)
 
 
 if __name__ == "__main__":
@@ -42,7 +24,4 @@ if __name__ == "__main__":
         check([14, 5, 6, 8, 15, 18, 12, 9, 4, 13, 1, 10, 11, 7, 16, 2, 17, 3])
     )  # False
     print(check([2, 3, 7, 6, 5, 1, 4, 8]))  # False
-    print(check([3, 1, 4, 5, 6, 2]))  # False WRONG
-    # print(check([3, 4, 5, 6, 2, 1]))  # ?
-
-    # print_permutations([1, 2, 3, 4, 5, 6], 10000)
+    print(check([3, 1, 4, 5, 6, 2]))  # False
