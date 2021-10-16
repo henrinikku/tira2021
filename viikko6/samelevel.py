fbf0895f03924c89703027da43557d9a11b9a9f2
Henri Nikku <henrinikku1@gmail.com>
2021-10-16 17:23:52 +0300

    Add 6.4

---
+++ viikko6/samelevel.py
@@ -0,0 +1,26 @@
+from collections import Counter, namedtuple
+
+Node = namedtuple("Node", ["left", "right"])
+
+
+def count(node: Node, level: int):
+    counter = Counter()
+
+    def _count(current_node: Node, current_level: int):
+        if current_node is None:
+            return 0
+
+        counter[current_level] += 1
+        _count(current_node.left, current_level + 1)
+        _count(current_node.right, current_level + 1)
+
+    _count(node, 1)
+    return counter[level]
+
+
+if __name__ == "__main__":
+    tree = Node(None, Node(Node(None, None), Node(None, None)))
+    print(count(tree, 1))  # 1
+    print(count(tree, 2))  # 1
+    print(count(tree, 3))  # 2
+    print(count(tree, 4))  # 0
