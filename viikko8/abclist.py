32de2d85f9f8df149d34337fa9997666a93b73d0
Henri Nikku <henrinikku1@gmail.com>
2021-11-07 11:39:09 +0200

    Add 8.3

---
+++ viikko8/abclist.py
@@ -0,0 +1,16 @@
+def _create(n: int, s: str):
+    if len(s) == n:
+        yield s
+    else:
+        for c in "ABC":
+            yield from _create(n, s + c)
+
+
+def create(n: int):
+    return list(_create(n, ""))
+
+
+if __name__ == "__main__":
+    print(create(1))  # [A,B,C]
+    print(create(2))  # [AB,AC,BA,BC,CA,CB]
+    print(len(create(5)))  # 48
